ea14c6c9cba40e528eb9cb874fe5dd0da3bcdbed
Mousera1 <163630449+Mousera1@users.noreply.github.com>
2024-08-21 13:12:57 +0530

    Update Task23EmployeeTable.java

--- Task23EmployeeTable.java
+++ Task23EmployeeTable.java
@@ -13,7 +13,7 @@ public class Task23EmployeeTable {
 		
 		//2.create statement
 		Statement stmt = con.createStatement();
-		String s = "UPDATE employeesalary SET empname = 'Tharun' WHERE empcode=105";
+		String s = "INSERT INTO EmployeeSalary (empcode, empname, empage, esalary) VALUES (101,'Jenny',25, 10000),(102,'Jacky',30, 20000), (103,'Joe',20, 40000), (104,'John',40, 80000), (105,'Shameer',25, 90000)";
 		//3.execute statement
 		
 		stmt.execute(s);
